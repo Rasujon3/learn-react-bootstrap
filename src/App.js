887aacffd20702ca5b03ef0af442e86a11cbb803
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-26 04:20:48 +0600

    React Fragment <> </>

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import News from '../src/components/News/News';
 import Header from '../src/components/Header/Header';
 import TopHeadline from '../src/components/TopHeadline/TopHeadline';
+import Fragment from '../src/components/Fragment/Fragment';
 
 function App() {
   return (
     <div>
       <h2>React Bootstrap</h2>
       <Header></Header>
+      <Fragment></Fragment>
       <TopHeadline></TopHeadline>
       {/* <News></News> */}
 
